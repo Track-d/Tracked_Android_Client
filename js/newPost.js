f26df872504cbe8cced7e2e30ff15c629dcc4807
gnn2 <gnn2@uw.edu>
2015-05-13 19:17:20 -0700

    fix mapview geolocation, add link to event page, added stuff for submission

--- js/newPost.js
+++ js/newPost.js
@@ -5,4 +5,11 @@ function addImage() {
 function activateItem(){
 	console.log("hi");
 	 jQuery(this).toggleClass('active');
+	  
+}
+
+function submitForm() {
+	alert("submit:http://tracked-server-dev.elasticbeanstalk.com/post_event");
+	var formData = JSON.stringify(jQuery('#form-inline').serializeArray()); // store json string
+	console.log(formData); 
 }
