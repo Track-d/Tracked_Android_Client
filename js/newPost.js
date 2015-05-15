8cd97dbbc31e2d352f72a9bbbbae3e4f8fcf3354
gnn2 <gnn2@uw.edu>
2015-05-15 11:42:41 -0700

    mapview, submission form

--- js/newPost.js
+++ js/newPost.js
@@ -7,9 +7,23 @@ function activateItem(){
 	 jQuery(this).toggleClass('active');
 	  
 }
-
+function validateForm(){
+	console.log("hi");
+	$('#name').validate();
+	console.log(a);
+    //var b=document.forms["Form"]["answer_b"].value;
+    //var c=document.forms["Form"]["answer_c"].value;
+    //var d=document.forms["Form"]["answer_d"].value;
+    if (a==null)
+      {
+      alert("Please Fill All Required Field");
+      return false;
+      }
+}
+	
 function submitForm() {
 	alert("submit:http://tracked-server-dev.elasticbeanstalk.com/post_event");
+	$("#name").validate();
 	var formData = JSON.stringify(jQuery('#form-inline').serializeArray()); // store json string
 	console.log(formData); 
 }
