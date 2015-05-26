5021772ecdd7e1d2cf67c1109e3926c5598eadab
gnn2 <gnn2@uw.edu>
2015-05-25 21:16:12 -0700

    merging michelles rso page branch

--- js/newPost.js
+++ js/newPost.js
@@ -57,8 +57,8 @@ $(document).ready(function() {
     });
 });*/
 function validateForm() {
-	console.log("hi");
-	$('#subForm').validate({
+	//console.log("hi");
+	return $('#subForm').validate({
 		 errorElement: "div",
 		 wrapper: 'div',
 		 rules: {
@@ -81,7 +81,8 @@ function validateForm() {
 			   isTime:true
 		   },
 		   "e_time": {
-			   isTime:true
+			   required: true,
+			   isTime:true,
 		   },
 		   "location": {
 			   required: true,
@@ -111,9 +112,12 @@ function validateForm() {
 				error.appendTo(element.next('.error_label'));
 			}
 		}
+		
 	});
+	
 }
 
+//add validator method for date
 $.validator.addMethod(
     "isDate",
     function(value, element) {
@@ -124,19 +128,76 @@ $.validator.addMethod(
     "Please enter a valid date in the format dd/mm/yyyy."
 );
 
+//add validator method for time 
 $.validator.addMethod(
     "isTime",
     function(value, element) {
-		console.log("check date");
-        var reg = /^(([0-1]?[0-2])|([2][0-3])):([0-5]?[0-9])(a|p)m?$/i;
+		console.log("check time");
+        var reg = /^(0?[1-9]|1[012])(:[0-5]\d)(a|p)m?$/i;
+		console.log(reg.test(value))
 		return reg.test(value);   
     },
     "Please enter a valid time in the format hh:mmAM/PM."
 );
 
 function submitForm() {
-	alert("submit:http://tracked-server-dev.elasticbeanstalk.com/post_event");
-	$("#name").validate();
-	var formData = JSON.stringify(jQuery('#form-inline').serializeArray()); // store json string
-	console.log(formData); 
-}
+	if(validateForm()){
+		var formData = $(subForm).serializeObject();
+		   $.ajax({
+           type: "POST",
+           url: "http://tracked-server-dev.elasticbeanstalk.com/post_event",
+           dataType: "jsonp",
+           success: function (msg) {
+                   alert("Post worked");
+				  console.log("Post worked");
+           },
+		   error: function(request, status, error){
+			   console.log("Post did not work");
+				alert("Post did not work, "+status + error);
+		   },
+           data: formData
+       });
+		console.log(formData); 
+	}
+	
+};
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+	console.log(o);
+    return o;
+};
+
+google.maps.event.addDomListener(window, 'load', function () {
+		//console.log("hi");
+           
+			var options = {
+				componentRestrictions: {country: "us"}
+			};
+			 var places = new google.maps.places.Autocomplete(document.getElementById('location'),options);
+            google.maps.event.addListener(places, 'place_changed', function () {
+			//	console.log("in function");
+                var place = places.getPlace();
+				document.getElementById('lat').value = place.geometry.location.lat();
+				document.getElementById('long').value = place.geometry.location.lng();
+                var address = place.formatted_address;
+                var latitude =place.geometry.location.lat();
+                var longitude = place.geometry.location.lng();
+                var mesg = "Address: " + address;
+                mesg += "\nLatitude: " + latitude;
+                mesg += "\nLongitude: " + longitude;
+              //  alert(mesg);
+				//console.log("message");
+            });
+});
