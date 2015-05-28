4bee96c5b9007eee363551b9bbfd2e0710d873ed
gnn2 <gnn2@uw.edu>
2015-05-27 21:29:14 -0700

    submission form tuggle between event and place

--- js/newPost.js
+++ js/newPost.js
@@ -133,41 +133,54 @@ $.validator.addMethod(
     "isTime",
     function(value, element) {
 		console.log("check time");
-        var reg = /^(0?[1-9]|1[012])(:[0-5]\d)(a|p)m?$/i;
+        var reg = /^(0?[1-9]|1[012])(:[0-5]\d)\s(a|p)m?$/i;
 		console.log(reg.test(value))
 		return reg.test(value);   
     },
-    "Please enter a valid time in the format hh:mmAM/PM."
+    "Please enter a valid time in the format hh:mm AM/PM."
 );
 
 function submitForm() {
 	if(validateForm()){
-		var postUrl= "http://tracked-server-dev.elasticbeanstalk.com/post_event";
+		var postUrl= "https://trackd.info/post_event";
+		if(document.getElementById('place').checked){
+			postUrl ="https://trackd.info/post_place"
+			alert("new post url " + postUrl)
+		}
+		var d =  document.getElementById("date").value.replace(/\//g, "-") + " " + document.getElementById("s_time").value;
+		console.log(d);
+		var s = new Date(d);
+		var e = new Date(document.getElementById("date").value.replace(/\//g, "-") + " " + document.getElementById("e_time").value);
+		
+		document.getElementById('start_time').value = s.toJSON(); 
+		document.getElementById('end_time').value = e.toJSON(); 
+		console.log("start time should be: " + s);
+		console.log("end time should be: " + e);
 		var formData = $(subForm).serializeObject();
 		var obj = JSON.stringify(formData);
 		console.log(obj);
 		   $.ajax({
-			url: "http://tracked-server-dev.elasticbeanstalk.com/post_event",
+			url: postUrl,
            type: "POST",
 		   contentType: "application/json; charset=utf-8",
 		   data: obj,
 		   async: false,		   
-           cache: false,    //This will force requested pages not to be cached by the browser
+           cache: false, //This will force requested pages not to be cached by the browser
+		   crossDomain:true,
            success: function (msg) {
                   alert("Post worked"+ msg);
 				  console.log("Post worked");
-				 // location.reload();
 				 // window.location = "http://localhost:9006/TrackdClient/createNew.html";
+				
            },
 		   error: function(request, status, error){
 			   console.log("Post did not work");
 				alert("Post did not work, "+ request +status + error);
-				
 				//window.location = "http://localhost:9006/TrackdClient/createNew.html";
 		   },
            
        });
-	   self.location = "http://localhost:9006/TrackdClient/createNew.html";
+	    //self.location = "http://localhost:9006/TrackdClient/createNew.html";
 		console.log(formData); 
 	}
 	
@@ -190,10 +203,39 @@ $.fn.serializeObject = function()
 	console.log(o);
     return o;
 };
-
+function selectEvent(){
+	$("#date").show();
+	$("#fbLink").show();
+	$("#org_name").show();
+	$("#s_time").show();
+	$("#e_time").show();
+	$("#event_desc_short").show();
+	$( "#date" ).prop( "disabled", false );
+	$( "#fbLink" ).prop( "disabled", false );
+	$( "#org_name" ).prop( "disabled", false );
+	$( "#s_time" ).prop( "disabled", false );
+	$( "#e_time" ).prop( "disabled", false );
+	$( "#event_desc_short" ).prop( "disabled", false );
+};
+function selectPlace(){
+    $("#date").hide();
+	$( "#date" ).prop( "disabled", true );
+	$("#fbLink").hide();
+	$( "#fbLink" ).prop( "disabled", true );
+	$("#org_name").hide();
+	$( "#org_name" ).prop( "disabled", true );
+	$("#s_time").hide();
+	$( "#s_time" ).prop( "disabled", true );
+	$("#e_time").hide();
+	$( "#e_time" ).prop( "disabled", true );
+	$("#event_desc_short").hide();
+	$( "#event_desc_short" ).prop( "disabled", true );
+	
+};
+//$(function() {
+//    $( "#date" ).datepicker();
+//  });
 google.maps.event.addDomListener(window, 'load', function () {
-		//console.log("hi");
-           
 			var options = {
 				componentRestrictions: {country: "us"}
 			};
