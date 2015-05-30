18d870e9f05eae211d92d4d04fad5bae5ff8d5f9
Michelle Le <mxl@uw.edu>
2015-05-30 14:26:09 -0700

    linking listview to eventview is working

--- js/getAllEventDetails.js
+++ js/getAllEventDetails.js
@@ -1,37 +1,26 @@
-		var myApp = angular.module('myApp', []);
-		// myApp.factory('Data', function(){
-		// 	var savedData = null;
-		// 	function set(data) {
-		// 		savedData = data;
-		// 	}
+		var myApp = angular.module('myApp', ["ui.router"])
 
-		// 	function get() {
-		// 			return savedData;
-		// 	}
+		.config(function config($stateProvider, $urlRouterProvider) {
+			 // send to listView
+    		$urlRouterProvider.otherwise("/"); 
 
-		// 	return {
-		// 		set: set,
-		// 		get: get
-		// 	}
-		// });
-
-		myApp.factory('Data', function () {
-
-		    var data = "hi";
-
-		    return {
-		        get: function () {
-		            return data;
-		        },
-		        set: function (evt) {
-		            this.data = evt;
-		        }
-		    };
+			$stateProvider.state("index", {
+				url: "/",
+				controller: "EventsCtrlAjax",
+				templateUrl: "template-listView.html"
+			})
+		})
+		.config(function config($stateProvider) {
+			$stateProvider.state("oneEvent", {
+				url: "/eventView",
+				controller: "OneEvent",           
+				templateUrl: "template-eventView.html"
+			})
+		})
+		.service("OneEvt", function OneEvt() {
+			var eventItem = this;
+			eventItem.item = "Default"
 		});
-
-		myApp.controller('MainControl', function ($scope, $http, Data) {
-	  		$scope.events = []
-	  	});
 
 		// Model for all event objects
 	  	function evnt(id, name, sTime, eTime, loc_name, lat, longi, shortD, longD) {
@@ -46,13 +35,16 @@
 	  		this.e_longD = longD;
 	  	}
 
+		myApp.controller('MainControl', function ($scope, $http, OneEvt) {
+	  		// alert("MainControl");
+	  	});
 
-		myApp.controller('EventsCtrlAjax', function ($scope, $http, Data) {
-			$http.get('http://tracked-server-dev.elasticbeanstalk.com/events').
-		    success(function(data, status, headers, config) {
-			   
-			   	// load event objects into $scope.events
-			   	for(item in data.events) {
+		myApp.controller('EventsCtrlAjax', function ($scope, $http, OneEvt) {
+	 		$scope.events = []
+	 		//var JSON;
+			$http.get('trackd.info/events').
+		     	success(function(data, status, headers, config) {
+		        for(item in data.events) {
 			   		var temp = data.events[item];
 			   		$scope.events.push(
 			   			new evnt (
@@ -60,7 +52,7 @@
 				   			temp.event_name,
 				   			Date.parse(temp.start_time),
 				   			Date.parse(temp.end_time),
-				   			temp.loc_info.name,
+				   			temp.loc_info.location,
 				   			temp.loc_info.lat,
 				   			temp.loc_info.log,
 				   			temp.event_desc_short,
@@ -68,28 +60,76 @@
 			   			)
 		   			);			   		
 			   	}
+
 			   	$scope.getOneEvent = function(id){
 			   		for(var i = 0; i < $scope.events.length; i++) {
 			   			try {
 				   			if ($scope.events[i].e_id == id) {
-				   				Data.set($scope.events[i].e_name);
-				   				$scope.item = $scope.events[i].e_name;
-				   				alert("listView: " + Data.data);
+				   				OneEvt.item = $scope.events[i];
 				   			}
 			   			} catch (error) {
 			   				alert(error);
 			   			}
 			   		}
 			   	}
-		    }).
-		    error(function(data, status, headers, config) {
+
+			   	// console.log($scope.events);
+			   	$scope.$apply();
+		 	});	   	
+		 });
+
+
+		myApp.controller('OneEvent', function ($scope, OneEvt) {
+			// setting selected event
+			$scope.item = OneEvt.item;	
+		});
+
+
+
+
+		// myApp.controller('EventsCtrlAjax', function ($scope, $http, OneEvt) {
+		// 	$scope.events = []
+		// 	$http.get('/events').
+		//     success(function(data, status, headers, config) {
+			   
+		// 	   	// load event objects into $scope.events
+		// 	   	for(item in data.events) {
+		// 	   		var temp = data.events[item];
+		// 	   		$scope.events.push(
+		// 	   			new evnt (
+		// 	   				temp.event_id,
+		// 		   			temp.event_name,
+		// 		   			Date.parse(temp.start_time),
+		// 		   			Date.parse(temp.end_time),
+		// 		   			temp.loc_info.name,
+		// 		   			temp.loc_info.lat,
+		// 		   			temp.loc_info.log,
+		// 		   			temp.event_desc_short,
+		// 		   			temp.event_desc_long
+		// 	   			)
+		//    			);			   		
+		// 	   	}
+		// 	   	var chosen = this;
+		// 	   	$scope.getOneEvent = function(id){
+		// 	   		for(var i = 0; i < $scope.events.length; i++) {
+		// 	   			try {
+		// 		   			if ($scope.events[i].e_id == id) {
+		// 		   				OneEvt.item = $scope.events[i];
+		// 		   			}
+		// 	   			} catch (error) {
+		// 	   				alert(error);
+		// 	   			}
+		// 	   		}
+		// 	   	}
+
+		// 	   	// console.log($scope.events);
+		// 	   	$scope.$apply();
+		// //     }).
+		//     error(function(data, status, headers, config) {
 	   
-	    	});
-		});
+	 //    	});
+		// });
 
 
-		myApp.controller('OneEvent', function ($scope, Data) {
-			$scope.item = $scope.events[0].e_name;
-			alert("eventView: " + $scope.item);
-		});
+	
 
